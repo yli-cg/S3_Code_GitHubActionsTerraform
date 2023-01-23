37cbf011955a30846222a7a4e17699bbc0bca8fc
YL <yuanyuan.li2@inter.ikea.com>
2023-01-23 10:44:05 +0100

    terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ terraform {
     resource_group_name  = "ws-devops"
     storage_account_name = "cgmsgtf"
     container_name       = "tfstateazdevops"
-    key                  = "<your unique name>.tfstate"
+    key                  = "yli1.tfstate"
   }
 }
 
@@ -18,3 +18,32 @@ provider "azurerm" {
   features {}
 }
 
+#Get resource group
+data "azurerm_resource_group" "wsdevops" {
+  name = var.rg_name
+}
+
+resource "azurerm_app_service_plan" "sp1" {
+  name                = var.app_service_plan_name
+  location            = var.location
+  resource_group_name = data.azurerm_resource_group.wsdevops.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "website" {
+  name                = var.web_app_name
+  location            = var.location
+  resource_group_name = data.azurerm_resource_group.wsdevops.name
+  app_service_plan_id = azurerm_app_service_plan.sp1.id
+
+  site_config {
+    linux_fx_version = "NODE|16-lts"
+    scm_type         = "LocalGit"
+  }
+}
